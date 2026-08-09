0c73273b304592772736c2dc85bbb7be6be00d05
johannes280-coder <johannes280@gmail.com>
2026-08-09 20:03:46 +0200

    Connect app to Supabase

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://fqpysutdtdcxscbbrlsx.supabase.co',
+  supabaseAnonKey: 'sb_publishable_fzJGJt35UNpENXUUlkkfUA_NCB_IMjC',
 };
